4597297e1587f4cdccd2a8c6264f9a3dbe0490fa
Serhii Spitsyn <103833310+ShamansIT@users.noreply.github.com>
2024-09-29 21:43:59 +0000

    Lab 1 Start

---
+++ assignments/Lab/Week_01/read_csv.py
@@ -0,0 +1,9 @@
+import csv
+
+FILENAME= "data.csv"
+DATADIR = “where did you put it”
+
+with open (DATADIR + FILENAME, "rt") as fp:
+    reader = csv.reader(fp, delimiter=",")
+    for line in reader:
+        print (line)
